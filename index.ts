4797611a7d58613827d1468ed75e461800af314f
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2023-10-18 22:14:05 +0900

    add todos post method

--- index.ts
+++ index.ts
@@ -4,3 +4,15 @@ const app = server();
 app.listen(3000, () => {
  console.log("Bun Server started on port 3000");
 })
+
+app.post('/todos', (req, res) => {
+ if (typeof req.body === 'object') {
+  const todo: any = req.body.todo;
+  const todo_id = todo.id;
+  todosMap.set(todo_id, todo);
+  res.status(201).json(todo);
+ } else {
+  res.status(400).json({error: "Invalid todo"});
+ }
+})
+
